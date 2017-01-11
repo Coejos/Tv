8653e55cffeecf9e3c6c45b0e107d0fd2d102dec
FACOMP <FACOMP@LAB1-PC10>
2017-01-11 15:56:13 -0300

    tentativa de comint

---
+++ Tv/Tv.cpp
@@ -0,0 +1,12 @@
+#include "Tv.h"
+
+Tv::Tv()
+{
+    int canal;
+    char nome;
+}
+
+Tv::~Tv()
+{
+}
+
---
+++ Tv/Tv.h
@@ -0,0 +1,12 @@
+#ifndef TV_H
+#define TV_H
+
+class Tv
+{
+public:
+    Tv();
+    ~Tv();
+
+};
+
+#endif // TV_H
